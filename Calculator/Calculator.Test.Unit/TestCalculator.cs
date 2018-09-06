b942aa7f5ebe720e6ae590b20c3006b33ff5ddde
Danny Tran <201610981@uni.au.dk>
2018-09-06 11:53:19 +0200

    Tilføjet Calculator

---
+++ Calculator/Calculator.Test.Unit/TestCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test.Unit
+{
+    class TestCalculator
+    {
+        private Calculator uut_;
+
+        [Test]
+
+        public void deep()
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Add(2, 3), Is.EqualTo(5));
+        }
+    }
+}
