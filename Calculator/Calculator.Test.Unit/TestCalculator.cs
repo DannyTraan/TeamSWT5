6bfccb4d91eb6d45ddfb727b82f5ba54a54d26d4
Danny Tran <201610981@uni.au.dk>
2018-09-06 13:05:46 +0200

    Testcases tilføjet

--- Calculator/Calculator.Test.Unit/TestCalculator.cs
+++ Calculator/Calculator.Test.Unit/TestCalculator.cs
@@ -11,12 +11,28 @@ namespace Calculator.Test.Unit
     {
         private Calculator uut_;
 
-        [Test]
-
-        public void deep()
+        [SetUp]
+        public void Setup()
         {
-            var uut = new Calculator();
-            Assert.That(uut.Add(2, 3), Is.EqualTo(5));
+            uut_ = new Calculator();
+        }
+
+        [TestCase(3, 2, 5)]
+        [TestCase(5, 5, 10)]
+        [TestCase(20, 80, 100)]
+        [TestCase(-50, 20, -30)]
+        public void Deep_add(int a, int b, int result)
+        {
+            Assert.That(uut_.Add(a, b), Is.EqualTo(result));
+        }
+
+        [TestCase(20, 7, 13)]
+        [TestCase(-5, -2, -3)]
+        [TestCase(17, 64, -47)]
+        [TestCase(2, 3, -1)]
+        public void Deep_Subtract(int a, int b, int result)
+        {
+            Assert.That(uut_.Subtract(a, b), Is.EqualTo(result));
         }
     }
 }
